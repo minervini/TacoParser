2305916adec151946ce276daf846f821d112d394
Stefano D Minervini <minervini@mac.com>
2019-10-06 22:04:18 -0500

    added TacoBell class, modified TacoParser tests, wrote Parser method

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -10,9 +10,36 @@
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
-
             // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+            // TODO Implement
+
+            if (line == null)
+            {
+                return null;
+            }
+            TacoBell trackableTacoBell = new TacoBell();
+            Point Location = new Point();
+            var cells = line.Split(',');
+
+            if (cells.Length < 3 || line == null)
+            {
+                logger.LogError("Missing information" + line);
+                return null;
+            }
+
+            bool latitudeParseSuccess = double.TryParse(cells[0], out double latitude);
+            bool longitudeParseSuccess = double.TryParse(cells[1], out double longitude);
+
+            if (latitudeParseSuccess == false || longitudeParseSuccess == false)
+                return null;
+
+            Location.Latitude = latitude;
+            Location.Longitude = longitude;
+            string name = cells[2];
+            trackableTacoBell.Name = name;
+            trackableTacoBell.Location = Location;
+            return trackableTacoBell;
+
         }
     }
 }
