0022ce135f52606197bef36d72ba5fc84f4795d2
Stefano D Minervini <minervini@mac.com>
2019-10-06 20:23:39 -0500

    started building tests

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -12,18 +12,43 @@ namespace LoggingKata.Test
         }
 
         [Theory]
-        [InlineData("Example")]
+        [InlineData("34.280205,-86.217115,Taco Bell Albertvill...")]
+        [InlineData("34.992219,-86.841402,Taco Bell Ardmore...")]
+        [InlineData("90 , 180, Taco Bell")]
+        [InlineData("-90, -180, Taco Bell ")]
+        [InlineData("0, 0, Taco bell")]
         public void ShouldParse(string str)
         {
             // TODO: Complete Should Parse
+            // Arrange
+            TacoParser location = new TacoParser();
+
+            // Act
+            ITrackable actual = location.Parse(str);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.NotNull(actual.Name);
+            Assert.NotNull(actual.Location);
         }
 
         [Theory]
         [InlineData(null)]
         [InlineData("")]
+        [InlineData("90, 90")]
+        [InlineData("90, TacoBell")]
+        [InlineData("TacoBell")]
         public void ShouldFailParse(string str)
         {
             // TODO: Complete Should Fail Parse
+            // Arrange
+            TacoParser Location = new TacoParser();
+
+            // Act
+            ITrackable actual = Location.Parse(str);
+
+            // Assert
+            Assert.Null(actual);
         }
     }
 }
